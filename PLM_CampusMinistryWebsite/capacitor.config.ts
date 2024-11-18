8550ec77cf72837ee157ba8b6d812a5890134b55
yurialfrance <yurialfrance05@gmail.com>
2024-11-18 11:29:28 +0800

    Newn Ionic Angular Files

---
+++ PLM_CampusMinistryWebsite/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PLM_CampusMiniistryWebsite',
+  webDir: 'www'
+};
+
+export default config;
